91b7aa92304f8df1d980e3a88f2fdb71e4ed221d
jasmine-jp <61972863+jasmine-jp@users.noreply.github.com>
2022-08-07 00:11:06 +0900

    modify correct data

--- network.py
+++ network.py
@@ -32,27 +32,21 @@ class NeuralNetwork(nn.Module):
             nn.MaxPool2d(pool)
         )
 
-        self.norm = nn.Sequential(
-            nn.Flatten(2),
-            nn.BatchNorm1d(arr_size)
-        )
-
         self.stack = nn.Sequential(
             nn.AvgPool2d((len(self.rnnlist), 1)),
             nn.Flatten(),
-            nn.Linear(out_size, len(ansmap)+1)
+            nn.Linear(arr_size, len(ansmap)+1)
         )
     
     def forward(self, x):
         x = torch.stack(list(map(self.conv2d, x)))
-        x = self.norm(x)
         self.conv = x.detach().clone()
-        x = torch.stack(
-            [xi[(xi.max(1).values-xi.min(1).values).argsort()] for xi in x.transpose(1, 2)]
-        ).transpose(0, 1)
-        x = torch.stack(list(map(
-            lambda e: self.rnnlist[e[0]](e[1].reshape((10, arr_size, -1)), None)[0], enumerate(x)))
-        ).transpose(0, 1)
-        self.rnn = x[:, :, -1].detach().clone()
-        x = self.stack(x[:, :, -1])
+        x = torch.stack(list(map(lambda e:
+            self.rnnlist[e[0]](e[1].reshape((10, arr_size, -1)), None)[0],
+            enumerate(torch.stack(
+                [xi[(xi.max(1).values-xi.min(1).values).argsort()] for xi in x.transpose(1, 2)]
+            ).transpose(0, 1))
+        ))).transpose(0, 1)
+        self.rnn = x[:, :, :, -1].detach().clone()
+        x = self.stack(x[:, :, :, -1])
         return x
